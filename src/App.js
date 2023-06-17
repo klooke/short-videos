393e71740f7a00d5f453ae59cb0497aabbb1e877
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2023-06-17 17:26:26 -0300

    day4: create video props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import "./App.css";
 import VideoContainer from "./components/VideoContainer";
 
 export default function App() {
+  const videoPropA = {
+    url: "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z",
+    user: "@Usuário A",
+    description: "Descrição do video A.",
+    music: "Título da música A.",
+    likes: 111,
+    messages: 222,
+    shares: 333
+  }
+
+  const videoPropB = {
+    url: "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z",
+    user: "@Usuário B",
+    description: "Descrição do video B.",
+    music: "Título da música B.",
+    likes: 444,
+    messages: 555,
+    shares: 666
+  }
+
   return (
     <div className="App">
-      <VideoContainer />
+      <VideoContainer      
+        videosProps={[
+          videoPropA,
+          videoPropB
+        ]}
+      />
     </div>
   );
 }
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import Video from "./Video";
 import "./VideoContainer.css";
 
-export default function VideoContainer() {
+export default function VideoContainer({videosProps}) {
   return (
     <div className="Video_Container">
       <Video />
